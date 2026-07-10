d47407634dd360cc5b80f24e08166474f5e8c9a4
busterbn <busterbbn@gmail.com>
2026-07-10 16:34:44 +0200

    Remove app2 (paused AQM sensor + display experiment)
    The app2 copy was swept into the 2.0.0 release by git add -A. It is kept in
    history (recoverable via git checkout a76c850 -- project/app2) if the AQM/OLED
    work resumes.

--- project/app2/src/battery_monitor.h
+++
@@ -1,7 +0,0 @@
-#ifndef BATTERY_MONITOR_H_
-#define BATTERY_MONITOR_H_
-
-/* Read both batteries and publish a PUB_BAT_REPORT now. */
-void battery_monitor_force_report(void);
-
-#endif /* BATTERY_MONITOR_H_ */
--- project/app2/src/relay_current.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RELAY_CURRENT_H_
-#define _RELAY_CURRENT_H_
-
-void relay_current_init(void);
-
-#endif /* _RELAY_CURRENT_H_ */
